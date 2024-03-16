badec3ab418a9bf596f47e8b94abe42e5d47b5f8
Dhvanit Monpara <monparadhvanit@gmail.com>
2024-03-16 16:06:37 +0530

    added login token

---
+++ profile.js
@@ -0,0 +1,100 @@
+let loginToken;
+
+const regForm = document.getElementById("registration-form");
+regForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  var username = document.getElementById("username").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var confirmPassword = document.getElementById("confirm-password").value;
+
+  var regErrorMessage = document.getElementById("registration-error-message");
+
+  if (
+    username.trim() === "" ||
+    email.trim() === "" ||
+    password.trim() === "" ||
+    confirmPassword.trim() === ""
+  ) {
+    regErrorMessage.textContent = "Please fill out all fields";
+  } else if (password !== confirmPassword) {
+    regErrorMessage.textContent = "Passwords do not match";
+  } else {
+    // storing data
+    let userData = {
+      userName: username,
+      emailAdd: email,
+      userPassword: password,
+    };
+    localStorage.setItem("userData", JSON.stringify(userData));
+
+    document.getElementById("username").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("confirm-password").value = "";
+    regErrorMessage.textContent = "";
+  }
+
+  loginSuccesfully();
+});
+
+const loginForm = document.getElementById("login-form");
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const loginErrorMessage = document.getElementById("login-error-message");
+  try {
+    let rawData = localStorage.getItem("userData");
+    let userData = JSON.parse(rawData);
+
+    let userEmail = document.getElementById("userEmail").value;
+    let storedUserPassword = document.getElementById("userPassword").value;
+
+    let userName = userData.userName;
+    let emailAdd = userData.emailAdd;
+    let userPassword = userData.userPassword;
+
+    if (userEmail.trim() === "" || storedUserPassword.trim() === "") {
+      loginErrorMessage.textContent = "Please fill out all fields";
+    } else if (storedUserPassword !== userPassword) {
+      loginErrorMessage.textContent = "Incorrect password";
+    } else if (userEmail !== emailAdd) {
+      loginErrorMessage.textContent = "Incorrect email address";
+    } else {
+      loginSuccesfully();
+      loginErrorMessage.textContent = "";
+    }
+  } catch {
+    loginErrorMessage.textContent =
+      "You doesn't have any account, try to register first.";
+  }
+});
+
+const heroPage = document.getElementById("heroPage");
+const loginPage = document.getElementById("loginPage");
+
+function loginSuccesfully() {
+  loginPage.style.display = "none";
+  loginToken = true;
+  sessionStorage.setItem("loginToken", loginToken);
+}
+
+const registrationField = document.querySelector(".reg-container");
+const loginField = document.querySelector(".login-container");
+
+function loginToggleFunc() {
+  loginField.classList.toggle("loginToggler");
+  registrationField.classList.toggle("loginToggler");
+}
+
+function regToggleFunc() {
+  loginField.classList.toggle("loginToggler");
+  registrationField.classList.toggle("loginToggler");
+}
+
+const loginToggleBtn = document.getElementById("loginToggleBtn");
+const regToggleBtn = document.getElementById("regToggleBtn");
+
+loginToggleBtn.addEventListener("click", loginToggleFunc);
+regToggleBtn.addEventListener("click", regToggleFunc);
--- script.js
+++ script.js
@@ -1,132 +1,64 @@
-const slides = document.querySelectorAll(".heroImage");
-
-var counter = 0;
-
-slides.forEach((slides, index) => {
-  slides.style.left = `${index * 100}%`;
-});
-
-const slideImage = () => {
-  slides.forEach((slide) => {
-    slide.style.transform = `translateX(-${counter * 100}%) rotateZ(-45deg)`;
-    if (counter >= 3) {
-      counter = 0;
-      slideImage();
-    }
-  });
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-  setInterval(() => {
-    counter++;
-    slideImage();
-  }, 6000);
-});
-
-// login page
-
-const regForm = document.getElementById("registration-form");
-regForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  var username = document.getElementById("username").value;
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-  var confirmPassword = document.getElementById("confirm-password").value;
-
-  var regErrorMessage = document.getElementById("registration-error-message");
-
-  if (
-    username.trim() === "" ||
-    email.trim() === "" ||
-    password.trim() === "" ||
-    confirmPassword.trim() === ""
-  ) {
-    regErrorMessage.textContent = "Please fill out all fields";
-  } else if (password !== confirmPassword) {
-    regErrorMessage.textContent = "Passwords do not match";
-  } else {
-    // Simulated registration process (here you can add your actual registration logic)
-    alert(
-      "Registration successful!\nUsername: " +
-        username +
-        "\nEmail: " +
-        email +
-        "\nPassword: " +
-        password
-    );
-
-    // storing data
-    let userData = {
-      userName: username,
-      emailAdd: email,
-      userPassword: password,
-    };
-    localStorage.setItem("userData", JSON.stringify(userData));
-
-    document.getElementById("username").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("password").value = "";
-    document.getElementById("confirm-password").value = "";
-    regErrorMessage.textContent = "";
-  }
-
-  loginSuccesfully();
-});
-
-const loginForm = document.getElementById("login-form");
-loginForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  let userEmail = document.getElementById("userEmail").value;
-  let storedUserPassword = document.getElementById("userPassword").value;
-  let loginErrorMessage = document.getElementById("login-error-message");
-
-  let rawData = localStorage.getItem("userData");
-  let userData = JSON.parse(rawData);
-
-  let userName = userData.userName;
-  let emailAdd = userData.emailAdd;
-  let userPassword = userData.userPassword;
-
-  if (userEmail.trim() === "" || storedUserPassword.trim() === "") {
-    loginErrorMessage.textContent = "Please fill out all fields";
-  } else if (storedUserPassword !== userPassword || userEmail !== emailAdd) {
-    loginErrorMessage.textContent = "Email address or password is incorrect";
-  } else {
-    console.log("login successfully");
-    loginSuccesfully();
-  }
-});
-
-const heroPage = document.getElementById("heroPage");
-const loginPage = document.getElementById("loginPage");
+function resetUserData() {
+  localStorage.clear();
+  sessionStorage.clear();
+}
 
 function loginSuccesfully() {
   loginPage.style.display = "none";
   heroPage.style.display = "flex";
-}
-heroPage.style.display = "none";
-
-const registrationField = document.querySelector(".reg-container");
-const loginField = document.querySelector(".login-container");
-
-function loginToggleFunc() {
-  loginField.classList.toggle("loginToggler");
-  registrationField.classList.toggle("loginToggler");
+  loginToken = true;
+  sessionStorage.setItem("loginToken", loginToken);
 }
 
-function regToggleFunc() {
-  loginField.classList.toggle("loginToggler");
-  registrationField.classList.toggle("loginToggler");
+function isLoginCheck() {
+  try {
+    let isLogin = sessionStorage.getItem("loginToken");
+    if (isLogin) {
+      loginSuccesfully();
+    }
+  } catch {}
 }
 
-const loginToggleBtn = document.getElementById("loginToggleBtn");
-const regToggleBtn = document.getElementById("regToggleBtn");
+isLoginCheck();
 
-loginToggleBtn.addEventListener("click", loginToggleFunc);
-regToggleBtn.addEventListener("click", regToggleFunc);
+// Profile
 
-function resetUserData(){
-  localStorage.clear()
+const profileCon = document.getElementById("profile-con");
+const userIdActive = document.getElementById("user-id");
+const userIdDesactive = document.getElementById("user-id-open");
+
+function toggleUserProfile() {
+  profileCon.classList.toggle("active-profile");
 }
+
+userIdActive.addEventListener("click", toggleUserProfile);
+userIdDesactive.addEventListener("click", toggleUserProfile);
+
+// In the profile
+
+const profileUsername = document.getElementById("profile-username");
+const profileEmailAdd = document.getElementById("profile-email-add");
+const logOutBtn = document.getElementById("log-out-btn");
+
+function insertProfileData() {
+  try {
+    let rawDataProfile = localStorage.getItem("userData");
+    let dataProfile = JSON.parse(rawDataProfile);
+    let usernameData = dataProfile.userName;
+    let emailData = dataProfile.emailAdd;
+
+    profileUsername.innerText = usernameData;
+    profileEmailAdd.innerText = emailData;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+insertProfileData();
+
+function logoutAcc() {
+  resetUserData();
+  window.location.reload();
+}
+
+logOutBtn.addEventListener("click", logoutAcc);
